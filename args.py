68f9bf9622a726df88136e2d2ad2ba71b8b1777c
Michael Lynch <lynchmv@gmail.com>
2022-03-15 08:58:37 -0400

    Python arguments

---
+++ args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import sys
+
+print(f"Argument 0 is {sys.argv[0]}")
+print(f"Argument 1 is {sys.argv[1]}")
+print(f"Argument 2 is {sys.argv[2]}")
+print(f"Argument 3 is {sys.argv[3]}")
